b0c20600e2d2e175a44fd7d1bf1fad5e31cc7bb2
vohndernet <22381311+vohndernet@users.noreply.github.com>
2020-04-13 00:28:46 -0700

    Online Demo: MessageCenter implemented sans backend, userContainers refactored, and general refactoring
    Basics of text message format for messaging has changed from chat actions to textarea input. Awaiting backend to send message out, but message  now shows up under individual user.
    
    General code refactoring: separate rendering functions for components of demo: user, message center, message animation (in-progress)
    
    Current Issues/Todos: user computers do not remain in position (restart in "new" position) upon onChange of text input --> lack of continuity to be addressed.
    
    Plane animation still in progress.

--- src/OnlineDemo.js
+++ src/OnlineDemo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Anime from 'react-anime';
-import { faDesktop, faGrinAlt, faServer, faCookie, faHandPaper, faHandScissors, faHandRock, faPaperPlane} from '@fortawesome/free-solid-svg-icons';
+import { faDesktop, faGrinAlt, faServer, faCookie, faHandPaper, faHandScissors, faHandRock, faPaperPlane, faWifi} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
@@ -8,146 +8,197 @@ class OnlineDemo extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            actionNames: ['Rock', 'Paper', 'Scissors', 'a Cookie', 'a Plane'],
-            actionSymbols: [faHandRock, faHandPaper, faHandScissors, faCookie, faPaperPlane],
-            actionColors: []
+            // actionNames: ['Rock', 'Paper', 'Scissors', 'a Cookie', 'a Plane'],
+            // actionSymbols: [faHandRock, faHandPaper, faHandScissors, faCookie, faPaperPlane],
+            // actionColors: []
+            message: "",
+            displayedMessage: "", //change upon end of plane animation
+            hasSentMessage: false,
+            sentCount: 0,
+            //eceivedUsers: [],
+            receivedMessages: ["i'm abby", "is this mael", "value this!", "pragmatic man, who do you think u are", "", ""]
+            //^^ messages from each user: Backend!
         }
     }
 
-    generateMessage = (userName, actionName) => {
-        let message = userName + " sent you " + actionName + '!';
-        return message;
+    handleChange = (e) =>
+    {
+      this.setState({message: e.target.value});
     }
 
-    handleAction = (chosenSymbol) => {
-        //this.socket.emit("user connected", {message: enteredName})
-        for(const [index, value] of this.state.actionSymbols.entries()){
-            if(chosenSymbol == value)
-            {
-                let actionType = this.state.actionNames[index];
-                let messageToSend = this.generateMessage(this.props.name, actionType);                
-                //SEND mesageToSend TO BACKEND
-            }
-        }
+    handleSubmit = (e) =>
+    {
+    //   send to backend as user, msg
+    //project onto screen
+        this.setState({hasSentMessage: true})
+        this.setState({displayedMessage: this.state.message})
     }
-    
-  render(){
-    let x = 10;
-    let y = 200;
-      const otherUsers = []
-      const otherNames = []
-      const others = this.props.others; 
-      const name = this.props.name; 
 
-    const actionButtons = []
+    renderSubmitButton = () => {
+        
+        return(
+            <p class="buttons">
+                <button class="button is-medium is-rounded" type="submit" onClick={this.handleSubmit}>
+                    <span>Send!</span> 
+                    <span class="icon is-small">
+                        <FontAwesomeIcon icon={faPaperPlane}></FontAwesomeIcon>
+                        </span>
+                </button>
+            </p>
+        );
+    }
 
-      for(const [index, value] of this.state.actionSymbols.entries()){
-        actionButtons.push(
-            <button class="button is-medium" onClick={this.handleAction}>
-                <span class="icon is-small">
-                    <FontAwesomeIcon icon={value}></FontAwesomeIcon>
-                    </span>
-            </button>
-        )
-      }
+    renderServer = () => {
+            return(
+                <span className="fa-layers fa-fw">
+                 <Anime easing="linear" loop={true} direction="alternate" opacity={['100%', '10%']}>
+                        <FontAwesomeIcon icon={faWifi} color="#d5f5ee" size="5x" flip="vertical" transform="down-12" ></FontAwesomeIcon>
+                </Anime>
+                <FontAwesomeIcon icon={faServer} size="6x" transform="" ></FontAwesomeIcon>
+            </span>
+            );
+    }
 
-      for(const [index, value] of others.entries()){
-      if(index != 0)
+    // renderUser = (name) => {
+    //     return(
+    //         <div className="userContainer" style={     {left: '40%', top:'100px', textAlign: 'center' }           }>
+    //                     <Anime easing="linear"  duration={1000} loop={true} direction="alternate" 
+    //                         translateY={[Math.random()*10+3, Math.random()*10+3]} 
+    //                         translateX={[Math.random()*10+3, Math.random()*10+3]}
+    //                     >        
+    //                         <span className="fa-layers fa-fw">
+    //                             <FontAwesomeIcon icon={faDesktop} size="4x" transform="left-2 down-6" ></FontAwesomeIcon>
+    //                             <FontAwesomeIcon icon={faGrinAlt} size="2x" transform="right-6 down-9" ></FontAwesomeIcon>
+    //                         </span>
+    //                         <p>{this.state.message}</p>
+                                            
+    //                         <div><p style={{position: 'absolute', right: '9px', top: '50px'}}>{name}</p></div>
+    //                     </Anime>
+                          
+    //                 </div>
+    //     );
+    // }
 
-        { let xSign = Math.random() < 0.5 ? -1 : 1;
-            let ySign = Math.random() < 0.5 ? -1 : 1;
-            
-            x = xSign*Math.random()*200 + 1;
-        y = ySign*Math.random()*200 + 100;
+    renderUser1 = (value, message, x, y) => {
+        
+
+        if(value == this.props.name)
+        {
+            x = 100;
+            y = -340
         }
-          otherUsers.push(
-        //   10, 200
-            <span className="userContainer" style={     {left: x, top:y }           }>
-                <Anime easing="linear"  duration={1000} loop={true} direction="alternate" 
+
+        return(
+
+            <Anime easing="linear"  duration={1000} loop={true} direction="alternate" 
                     translateY={[Math.random()*10+1, Math.random()*10+1]} 
                     translateX={[Math.random()*10+1, Math.random()*10+1]}
                 >
-        
+            <div className="userContainer" style={     {left: x, top:y }           }>
                     <span className="fa-layers fa-fw">
-                        <FontAwesomeIcon icon={faDesktop} size="4x" transform="left-2 down-6" ></FontAwesomeIcon>
-                        <FontAwesomeIcon icon={faGrinAlt} size="2x" transform="right-6 down-9" ></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faDesktop} size="4x" transform="left-6 down-8" ></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faGrinAlt} size="2x" transform="left-1 down-14" ></FontAwesomeIcon>
                     </span>                    
-                    <div><p style={{position: 'absolute', right: '9px', top: '50px'}}>{value}</p></div>
-                </Anime>
-            </span>
-            )
+                    <div><p style={{position: 'relative', top: '-40px'}}>{value}</p></div>
+                    <div><p style={{position: 'relative', top: '50px', fontSize: '14px'}}>{message}</p></div>
+                
+            </div>
+            </Anime>
+    );
+    }
 
-            otherNames.push(
-                <option>{value}</option>
-            )
+    renderMessageCenter = () => {
+        return(
+            <div class="container" className="messageCenterContainer">
+                    <div class="card">
+                    <div class="card-content">                        
+                    <h2>Say something!</h2>
+                    <div class="control">
+                    <textarea class="textarea has-fixed-size" placeholder="Say something!" type="text" onChange={e => this.handleChange(e)} value={this.state.message} ></textarea>
+                    </div>
+                        {this.renderSubmitButton()}
+                    </div>
+                    </div>
+            </div>
+
+        )
+    }
+
+    renderPlane = () => {
+       
+        if(this.state.hasSentMessage)
+        {
+            if(this.state.sentCount > 1400)
+            {
+                this.setState({hasSentMessage: false})
+                this.setState({sentCount: 0})
+            }
+//            this.setState({sentCount: this.state.sentCount + 1})
+        return(
+            <React.Fragment>
+            <span className="fa-layers fa-fw">
+             {/* <Anime easing="linear" loop={true} direction="alternate" opacity={['100%', '10%']}>
+                    
+            </Anime> */}
+            <Anime easing="linear" duration={1400} loop={false} translateX={500} translateY={-300} opacity={0}>
+            <FontAwesomeIcon icon={faPaperPlane} size="4x" transform={"up-20 left-25"}></FontAwesomeIcon>
+            </Anime>
+        </span>
+        </React.Fragment>
+        );
+        }
+        return(<p>No Plane</p>);
+    }
+    
+  render(){
+      const otherUsers = [];
+      //const otherNames = [];
+      const others = this.props.others; 
+      const name = this.props.name; 
+
+
+    // const actionButtons = []
+
+    //   for(const [index, value] of this.state.actionSymbols.entries()){
+    //     actionButtons.push(
+    //         <button class="button is-medium" onClick={this.handleAction}>
+    //             <span class="icon is-small">
+    //                 <FontAwesomeIcon icon={value}></FontAwesomeIcon>
+    //                 </span>
+    //         </button>
+    //     )
+    //   }
+
+      for(const [index, value] of others.entries()){
+      if(index != 0) {
+        let xSign = Math.random() < 0.5 ? -1 : 1;
+        let ySign = Math.random() < 0.5 ? -1 : 1;
+        
+        let x = xSign*(index)*30 + 1;
+        let y = ySign*(index/2)*70;
+
+          otherUsers.push(
+            this.renderUser1(value, this.state.receivedMessages[index], x, y));
+            
       }
+    }
 
 
       return(
           <section class="hero is-fullheight is-primary" >
               <div class="hero-head">
                     <h2 class="subtitle" style={{padding: '40px'}}>Hooray, you did it! This is everyone connected right now with you. Let's talk with them! </h2>
-                    <div className="userContainer" style={     {left: '40%', top:'100px', textAlign: 'center' }           }>
-                        <Anime easing="linear"  duration={1000} loop={true} direction="alternate" 
-                            translateY={[Math.random()*10+1, Math.random()*10+1]} 
-                            translateX={[Math.random()*10+1, Math.random()*10+1]}
-                        >        
-                            <span className="fa-layers fa-fw">
-                                <FontAwesomeIcon icon={faDesktop} size="4x" transform="left-2 down-6" ></FontAwesomeIcon>
-                                <FontAwesomeIcon icon={faGrinAlt} size="2x" transform="right-6 down-9" ></FontAwesomeIcon>
-                            </span>
-                                            
-                            <div><p style={{position: 'absolute', right: '9px', top: '50px'}}>{name}</p></div>
-                        </Anime>
-
-                            <FontAwesomeIcon icon={faServer} size="6x" transform="right-50 up-10" ></FontAwesomeIcon>
-                    </div>
-            
-
-
-                    <div class="container" className="controlPanelContainer">
-                    <div class="card">
-                    {/* <header class="card-header">
-                        <h2 class="subtitle ">Let's talk to them!</h2>
-                    </header> */}
-                    <div class="card-content"> {/*box notification is-warning */}                        
-                                    
-                        <div className="field">
-                            <p className="control">
-                                <p><strong>You're talking to: </strong></p>
-                                <span className="select is-centered">
-                                    <select>
-                                        <option>Find someone!</option>
-                                        {otherNames}                                              
-                                    </select>
-                                </span>
-                            </p>
-                        </div>
-
-                        <p class><strong>Send them something!</strong></p>
-                        <p class="buttons">
-                        {actionButtons}
-                        </p>
-                    </div>
-                    </div>
-                    </div>
-
-
-
-                  
-
-
-
-
+                    {this.renderServer()}
+            </div>
+            <div class="hero-body">
+            {this.renderMessageCenter()}
+                {this.renderUser1(name, this.state.displayedMessage)}
+                {this.renderPlane()}
+                    {otherUsers}
+                
             </div>
 
-            <div className="onlineContainer">
-
-            {otherUsers}
-            </div>
             <div class="hero-footer" style={{paddingBottom: '50px'}}>
-            {/* here lies foot */}
-
             </div>
           </section>
       )
